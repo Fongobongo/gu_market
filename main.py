956585cc890d550ead3d30528e9ef944ee9b777d
Fongobongo <betaplayer@mail.ru>
2022-05-18 11:48:56 +0500

    public version

--- main.py
+++ main.py
@@ -6,22 +6,22 @@ from tqdm import tqdm
 from datetime import datetime
 
 
-# get total amount of cards
 def get_amount_of_cards():
+
     url_total_cards = 'https://api.godsunchained.com/v0/proto?page=1&perPage=0'
     total_cards_amount = requests.get(url_total_cards).json()['total']
     return total_cards_amount
 
 
-# get all cards
 def get_cards_list(amount):
+
     url_all_cards = f'https://api.godsunchained.com/v0/proto?page=1&perPage={amount}'
     cards_dict = requests.get(url_all_cards).json()['records']
     return cards_dict
 
 
-# save cards to file
 def save_cards_to_file(cards_list):
+
     card_ids = {}
 
     with open('cards.txt', 'w', encoding='utf-8') as f:
@@ -34,6 +34,7 @@ def save_cards_to_file(cards_list):
 
 
 def get_crypto_prices():
+
     currencies_url = 'https://api.coingecko.com/api/v3/simple/price?ids=gods-unchained,ethereum&vs_currencies=usd'
 
     currencies = requests.get(currencies_url).json()
@@ -44,78 +45,58 @@ def get_crypto_prices():
     return currency_eth, currency_gods
 
 
-def get_eth_prices(card_id, eth):
-    prices = []
+def get_cards_prices(card_id, eth, gods):
 
-    url = f'https://api.x.immutable.com/v1/orders?buy_token_type=ETH&direction=asc&include_fees=true&order_by=buy_quantity&page_size=200&sell_metadata=%7B%22proto%22%3A%5B%22{card_id}%22%5D%2C%22quality%22%3A%5B%22Meteorite%22%5D%7D&sell_token_address=0xacb3c6a43d15b907e8433077b6d38ae40936fe2c&status=active'
+    prices_eth = []
+    prices_gods = []
+
+    urls = {
+        'eth': f'https://api.x.immutable.com/v1/orders?buy_token_type=ETH&direction=asc&include_fees=true&order_by=buy_quantity&page_size=200&sell_metadata=%7B%22proto%22%3A%5B%22{card_id}%22%5D%2C%22quality%22%3A%5B%22Meteorite%22%5D%7D&sell_token_address=0xacb3c6a43d15b907e8433077b6d38ae40936fe2c&status=active',
+        'gods': f'https://api.x.immutable.com/v1/orders?buy_token_address=0xccc8cb5229b0ac8069c51fd58367fd1e622afd97&direction=asc&include_fees=true&order_by=buy_quantity&page_size=200&sell_metadata=%7B%22proto%22%3A%5B%22{card_id}%22%5D%2C%22quality%22%3A%5B%22Meteorite%22%5D%7D&sell_token_address=0xacb3c6a43d15b907e8433077b6d38ae40936fe2c&status=active'
+    }
+
     headers = {"Accept": "*/*"}
 
-    complete = False
+    for k, v in urls.items():
 
-    while complete is False:
+        complete = False
 
-        orders = requests.get(url, headers=headers)
+        while complete is False:
 
-        if orders.reason == 'OK':
-            orders = orders.json()
-            complete = True
+            orders = requests.get(v, headers=headers)
 
-            if orders['result']:
-                orders = orders['result']
-            elif orders['remaining'] == 0:
-                return
-            elif orders['remaining'] != 0:
-                print(orders)
+            if orders.reason == 'OK':
+                orders = orders.json()
+                complete = True
 
-            for order in orders:
-                prices.append(round(float(order['buy']['data']['quantity_with_fees']) / 10 ** 18 * eth, 2))
+                if orders['result']:
+                    orders = orders['result']
+                elif orders['remaining'] == 0:
+                    return None, None
 
-            return sorted(prices)
-        elif orders.reason == 'Too Many Requests':
-            time.sleep(60)
-        else:
-            print(orders.reason)
-            return
+                for order in orders:
+                    if k == 'eth':
+                        prices_eth.append(round(float(order['buy']['data']['quantity_with_fees']) / 10 ** 18 * eth, 2))
+                    elif k == 'gods':
+                        prices_gods.append(round(float(order['buy']['data']['quantity_with_fees']) / 10 ** 18 * gods, 2))
+
+                return sorted(prices_eth), sorted(prices_gods)
+
+            elif orders.reason == 'Too Many Requests':
+                time.sleep(60)
+            else:
+                print(orders.reason)
+                return None, None
 
 
-def get_gods_prices(card_id, gods):
-    prices = []
+def split_cards_to_chunks(dict_of_cards):
 
-    url = f'https://api.x.immutable.com/v1/orders?buy_token_address=0xccc8cb5229b0ac8069c51fd58367fd1e622afd97&direction=asc&include_fees=true&order_by=buy_quantity&page_size=200&sell_metadata=%7B%22proto%22%3A%5B%22{card_id}%22%5D%2C%22quality%22%3A%5B%22Meteorite%22%5D%7D&sell_token_address=0xacb3c6a43d15b907e8433077b6d38ae40936fe2c&status=active'
-    headers = {"Accept": "*/*"}
-
-    complete = False
-
-    while complete is False:
-
-        orders = requests.get(url, headers=headers)
-
-        if orders.reason == 'OK':
-            orders = orders.json()
-            complete = True
-
-            if orders['result']:
-                orders = orders['result']
-            elif orders['remaining'] == 0:
-                return
-            elif orders['remaining'] != 0:
-                print(orders)
-
-            for order in orders:
-                prices.append(round(float(order['buy']['data']['quantity_with_fees']) / 10 ** 18 * gods, 2))
-
-            return sorted(prices)
-        elif orders.reason == 'Too Many Requests':
-            time.sleep(60)
-        else:
-            print(orders.reason)
-            return
+    return [{k: dict_of_cards[k]} for k in dict_of_cards.keys()]
 
 
 def calculate_profit(card_id, card_name):
 
-    card_prices_in_eth = get_eth_prices(card_id, eth_price)
-    card_prices_in_gods = get_gods_prices(card_id, gods_price)
+    card_prices_in_eth, card_prices_in_gods = get_cards_prices(card_id, eth_price, gods_price)
 
     if card_prices_in_eth and card_prices_in_gods:
         profit_usd = round(card_prices_in_eth[0] - card_prices_in_gods[0], 2)
@@ -129,20 +110,37 @@ def calculate_profit(card_id, card_name):
         profit_crypto = 'GODS'
         profit_rate = round(card_prices_in_gods[0] / card_prices_in_eth[0] * 100 - 100, 2)
 
-    # print(f'[{card_name}]: [Profit in {profit_crypto}: +{abs(profit_usd)}$ ({profit_rate}%)], ETH: {card_prices_in_eth[0]}$ vs GODS: {card_prices_in_gods[0]}$')
-
     return [card_name, profit_crypto, abs(profit_usd), profit_rate, card_prices_in_eth[0], card_prices_in_gods[0]]
 
 
+def calculate_top_profit(card_dict):
+
+    for card_id, card_name in card_dict.items():
+        card_profit = calculate_profit(card_id, card_name)
+
+        if card_profit:
+            return card_profit
+
+
+def run_multiple_requests(chunks_list):
+
+    profit_list = []
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=6) as e:
+        fut = [e.submit(calculate_top_profit, card) for card in chunks_list]
+        for r in tqdm(concurrent.futures.as_completed(fut), total=len(chunks_list)):
+            profit_list.append(r.result())
+
+    return profit_list
+
+
 def get_top_profit(list_of_prices):
-    # if list_of_prices is None:
-    #     return
 
     profit_eth = []
     profit_gods = []
 
     for item in list_of_prices:
-        if item is not None:
+        if item:
             if item[1] == 'ETH':
                 profit_eth.append(item)
             elif item[1] == 'GODS':
@@ -150,65 +148,49 @@ def get_top_profit(list_of_prices):
         else:
             continue
 
-    profit_eth_by_usd = []
     profit_eth_by_rate = []
-
-    profit_gods_by_usd = []
     profit_gods_by_rate = []
 
-    for card in sorted(profit_eth, key=lambda x: x[2], reverse=True)[:10]:
-        profit_eth_by_usd.append(f'[{card[0].ljust(20)}]: [Profit in {card[1]}: +{card[2]}$ ({card[3]}%)], [ETH: {card[4]}$ vs GODS: {card[5]}$]')
     for card in sorted(profit_eth, key=lambda x: x[3], reverse=True)[:10]:
-        profit_eth_by_rate.append(f'[{card[0].ljust(20)}]: [Profit in {card[1]}: +{card[3]}% ({card[2]}$)], [ETH: {card[4]}$ vs GODS: {card[5]}$]')
+        profit_eth_by_rate.append(f'[{card[0]}]: [Profit in {card[1]}: +{card[3]}% ({card[2]}$)], [ETH: {card[4]}$ vs GODS: {card[5]}$]')
 
-    for card in sorted(profit_gods, key=lambda x: x[2], reverse=True)[:10]:
-        profit_gods_by_usd.append(f'[{card[0].ljust(20)}]: [Profit in {card[1]}: +{card[2]}$ ({card[3]}%)], [GODS: {card[5]}$ vs ETH: {card[4]}$]')
     for card in sorted(profit_gods, key=lambda x: x[3], reverse=True)[:10]:
-        profit_gods_by_rate.append(f'[{card[0].ljust(20)}]:' + f'[Profit in {card[1]}: +{card[3]}% ({card[2]}$)], [GODS: {card[5]}$ vs ETH: {card[4]}$]')
+        profit_gods_by_rate.append(f'[{card[0]}]:' + f'[Profit in {card[1]}: +{card[3]}% ({card[2]}$)], [GODS: {card[5]}$ vs ETH: {card[4]}$]')
 
-    with open('profit_eth_by_usd.txt', 'w', encoding='utf-8') as f:
-        for item in profit_eth_by_usd:
-            f.write(str(item) + '\n')
+    return profit_eth_by_rate, profit_gods_by_rate
+
+
+def write_profit_to_file(eth, gods):
 
     with open('profit_eth_by_rate.txt', 'w', encoding='utf-8') as f:
-        for item in profit_eth_by_rate:
-            f.write(str(item) + '\n')
-
-    with open('profit_gods_by_usd.txt', 'w', encoding='utf-8') as f:
-        for item in profit_gods_by_usd:
+        for item in eth:
             f.write(str(item) + '\n')
 
     with open('profit_gods_by_rate.txt', 'w', encoding='utf-8') as f:
-        for item in profit_gods_by_rate:
+        for item in gods:
             f.write(str(item) + '\n')
 
 
 if __name__ == '__main__':
+
     start_time = datetime.now()
     print('Start time:', start_time)
+
     amount_of_cards = get_amount_of_cards()
+
     all_cards = get_cards_list(amount_of_cards)
-    cards = save_cards_to_file(all_cards)
+
+    tradeable_cards = save_cards_to_file(all_cards)
+
     eth_price, gods_price = get_crypto_prices()
 
-    profit = []
+    chunks = split_cards_to_chunks(tradeable_cards)
 
-    chunks = [{k: cards[k]} for k in cards.keys()]
+    profit = run_multiple_requests(chunks)
 
-    def calculate_top_profit(card_dict):
-        time.sleep(2)
-        for card_id, card_name in card_dict.items():
-            card_profit = calculate_profit(card_id, card_name)
+    eth_profit, gods_profit = get_top_profit(profit)
 
-            if card_profit:
-                return card_profit
+    write_profit_to_file(eth_profit, gods_profit)
 
-
-    with concurrent.futures.ThreadPoolExecutor(max_workers=12) as e:
-        fut = [e.submit(calculate_top_profit, card) for card in chunks]
-        for r in tqdm(concurrent.futures.as_completed(fut), total=len(chunks)):
-            profit.append(r.result())
-
-    get_top_profit(profit)
     print('End time:', datetime.now())
     print('Total time:', datetime.now() - start_time)
